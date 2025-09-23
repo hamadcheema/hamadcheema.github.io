dbb5302f9a7fa2b3e1979f18abda72df0801253b
hamadcheema <thatsmehammad@gmail.com>
2025-09-23 08:24:15 +0500

    Update post-card.js

--- JustCheck/js/components/post-card.js
+++ JustCheck/js/components/post-card.js
@@ -1,1 +1,20 @@
+// Post card component
+export function createPostCard(post) {
+  const div = document.createElement("div");
+  div.classList.add("post-card");
+
+  div.innerHTML = `
+    <div class="post-header">
+      <strong>@${post.userId}</strong>
+    </div>
+    <div class="post-media">
+      <img src="${post.media}" alt="post" />
+    </div>
+    <div class="post-caption">
+      <p>${post.caption}</p>
+    </div>
+  `;
+
+  return div;
+}
 
