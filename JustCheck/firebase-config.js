0f424ee1ef35b57dfa8e9f79b4abc013ee7fc0e1
hamadcheema <thatsmehammad@gmail.com>
2025-09-23 15:32:07 +0500

    Update firebase-config.js

--- JustCheck/firebase-config.js
+++ JustCheck/firebase-config.js
@@ -1,13 +1,14 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js';
-import { getDatabase } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js';
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBHQyKuiAgi831qANOkkWTNprdW1Pq6rbA",
   authDomain: "devchatbyhamad.firebaseapp.com",
   databaseURL: "https://devchatbyhamad-default-rtdb.firebaseio.com",
   projectId: "devchatbyhamad",
-  storageBucket: "devchatbyhamad.firebasestorage.app",
+  storageBucket: "devchatbyhamad.firebasestorage.app", // Still good to have for consistency
   messagingSenderId: "798871184058",
   appId: "1:798871184058:web:c735bea9756f8109149883",
   measurementId: "G-QTCCWC70QH"
@@ -18,4 +19,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
 
-export { auth, database };
+export { app, auth, database };
