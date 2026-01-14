17cb326ba8290b23f74f60396ab0648448bd44da
hamadcheema <thatsmehammad@gmail.com>
2026-01-15 02:47:11 +0500

    Add files via upload

---
+++ Portfolio/src/sections/info.section.tsx
@@ -0,0 +1,60 @@
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { IoMailOutline } from 'react-icons/io5';
+
+import { FloatingButton } from '../components/floating-button';
+import { GlowLink } from '../components/glow-box-link';
+import { TextHover } from '../components/text-hover.component';
+
+export const InfoSection = () => {
+	return (
+		<div className='hero-section'>
+			<a
+				href='https://github.com/Abdullahiqbal2021/portfolio-latest'
+				target='_blank'
+				rel='noopener noreferrer'
+				className='github-ribbon'
+			>
+				<div>⭐ Star this repo</div>
+				<span></span>
+			</a>
+			<FloatingButton
+				label='About Me'
+				className='first'
+				href='/#about-me'
+			/>
+			<p>Hi, I am</p>
+			<TextHover
+				text='ABDULLAH'
+				className='name'
+			/>
+			<p>Full Stack Developer</p>
+			<FloatingButton
+				label='Tech'
+				className='sec'
+				href='/#tech-stack'
+			/>
+
+			<div className='blur'></div>
+			<div className='bottom-bar'>
+				<GlowLink
+					href='https://github.com/Abdullahiqbal2021'
+					color='rgba(255, 255, 255, 0.4)'
+					icon={<FaGithub color='rgba(255, 255, 255,0.9)' />}
+					aria-label='github'
+				/>
+				<GlowLink
+					href='https://www.linkedin.com/in/muhammad-abdullah-iqbal/'
+					color='rgb(0, 160, 220, 0.6)'
+					icon={<FaLinkedinIn color='rgb(0, 160, 220)' />}
+					aria-label='linkedin'
+				/>
+				<GlowLink
+					href='mailto:muhammadabdullahiqbal2021@gmail.com'
+					icon={<IoMailOutline color='rgb(18, 122, 209)' />}
+					color=' rgb(18, 122, 209,0.7)'
+					aria-label='mail'
+				/>
+			</div>
+		</div>
+	);
+};
